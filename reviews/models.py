e7cabca78aa8bdd126143178557a4f822d7fe318
Madeline Anson <madelineganson@gmail.com>
2023-12-09 21:22:00 -0500

    added additional review entry fields

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Review(models.Model):
+    name = models.CharField(max_length=255)
+    email = models.EmailField()
+    phone_number = models.CharField(max_length=15)
+    review_text = models.TextField()
+
+    def __str__(self):
+        return self.review_text
+    
--- reviews/views.py
+++ reviews/views.py
@@ -1,20 +1,38 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
-from django.http import JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.schemas import DefaultSchema
-
-reviews = []
+from reviews.models import Review
 
 class AppDevClubReviewsView(APIView):
-    def post(self, request):
-        user_review = request.get('user_review', '')
-        #review to the array
-        reviews.append(user_review)
-        print(user_review)
-        return JsonResponse({'status': 'success'})
-    
     def get(self, request):
-        return Response({'user_reviews': reviews})
+        reviews = []
+        for review in Review.objects.all():  # Use all() to get all reviews
+            review_data = {
+                'name': review.name,
+                'email': review.email,
+                'phone_number': review.phone_number,
+                'review_text': review.review_text,
+            }
+            reviews.append(review_data)
+
+        return Response({'reviews': reviews})
+    
+class CreateAppDevClubReview(APIView):
+    def post(self, request):
+        name = request.data.get('name', '')
+        email = request.data.get('email', '')
+        phone_number = request.data.get('phone_number', '')
+        review_text = request.data.get('review_text', '')
+         
+        if review_text == '':
+            return Response({'message': 'failure'})
+
+        #add review to the array ORM
+        new_database_entry = Review(
+            name=name,
+            email=email,
+            phone_number=phone_number,
+            review_text=review_text
+        )
+        new_database_entry.save()
+        return Response({'message': 'success'})
